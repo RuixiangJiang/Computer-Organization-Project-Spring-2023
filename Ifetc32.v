d5bd1a8be2bd6f6cb19f38ead1b72f55105e10c9
RuixiangJiang <2037358823@qq.com>
2023-05-13 12:09:29 +0800

    hh

--- Ifetc32.v
+++ Ifetc32.v
@@ -1,27 +1,32 @@
 `timescale 1ns / 1ps
 module Ifetc32(
     output[31:0] Instruction, // the instruction fetched from this module to Decoder and Controller
-    output[31:0] branch_base_addr, // (pc + 4) to ALU which is used by branch type instruction
+    output[31:0] Branch_base_addr, // (pc + 4) to ALU which is used by Branch type instruction
     input[31:0] Addr_result, // the calculated address from ALU
-    input[31:0] Read_data_1, // the address of instruction used by jr instruction
+    input[31:0] Read_data_1, // the address of instruction used by Jr instruction
     input Branch, // current instruction is beq
     input nBranch, // current instruction is bnq
     input Jmp, // current instruction is jump
-    input Jal, // current instruction is jal
-    input Jr, // current instruction is jr
-    input Zero, // if the ALUresult is zero
+    input Jal, // current instruction is Jal
+    input Jr, // current instruction is Jr
+    input Zero, // if the ALUresult is Zero
     input clock,
     input reset,
-    output reg [31:0] link_addr // (pc + 4) to Decoder which is used by jal instruction
+    output reg [31:0] link_addr // (pc + 4) to Decoder which is used by Jal instruction
 );
     reg[31:0] PC, nextPC;
-    reg[31:0] jalPC;
-    assign branch_base_addr = PC + 4;
-    
+    assign Branch_base_addr = PC + 4;
+
+    prgrom instmem(
+        .clka(clock), // input wire clka
+        .addra(PC[15:2]), // input wire [13:0] addra
+        .douta(Instruction) // output wire [31:0] douta
+    );
+
     always @(*) begin
         // beq, bne
         if ((Branch && Zero) || (nBranch && ~Zero)) nextPC = Addr_result << 2;
-        // jr
+        // Jr
         else if (Jr) nextPC = Read_data_1 << 2;
         // other
         else nextPC = PC + 4;
@@ -30,7 +35,7 @@ module Ifetc32(
     always @(negedge clock) begin
         if (reset) PC <= 0;
         else begin
-            if (Jmp || Jal) PC <= {4'b0000, Instruction[25:0], 2'b00};
+            if (Jmp || Jal) PC <= {PC[31:28], Instruction[25:0], 2'b00};
             else PC <= nextPC;
         end
     end
