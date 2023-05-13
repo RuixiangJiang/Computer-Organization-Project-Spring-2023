955ab4477d262742f96d2b6ab6bfbe10dc2c06a3
SnowCharm <619022098@qq.com>
2023-05-13 11:29:09 +0800

    update for OJ

--- Controller.v
+++ Controller.v
@@ -1,35 +1,33 @@
 `timescale 1ns / 1ps
 
-//后面应该要统一调整下变量名
-module Controller(opcode, funct, Jr, Jmp, Jal, Branch, nBranch, 
-RegDST, MemtoReg, RegWrite, MemWrite, ALUSrc, Sftmd, I_format, ALUOp);
-    input[5:0] opcode;
-    input[5:0] funct;
-    output Jr;
-    output Jmp;
-    output Jal;
-    output Branch;
-    output nBranch;
-    output RegDST;
-    output MemtoReg;
-    output RegWrite;
-    output MemWrite;
-    output ALUSrc;
-    output Sftmd;
-    output I_format;
-    output[1:0] ALUOp; // R_type/I_type-2'b10 beq/bne-2'b01 lw/sw-2'b00
+module control32(Opcode, Function_opcode, Jr, RegDST, ALUSrc, MemtoReg, RegWrite, MemWrite, Branch, nBranch, Jmp, Jal, I_format, Sftmd, ALUOp);
+    input[5:0]   Opcode;            // instruction[31..26] - the high 6 bits of the instruction from the IFetch module
+    input[5:0]   Function_opcode;  	// instructions[5..0] - the low 6 bits of the instruction from the IFetch module, used to distinguish instructions in r-type
+    output       Jr;         	 // 1 if the current instruction is jr, 0 otherwise
+    output       RegDST;          // 1 if the destination register is rd, 0 if the destination register is rt
+    output       ALUSrc;          // 1 if the second operand (Binput in ALU) is an immediate value (except for beq, bne), otherwise the second operand comes from the register
+    output       MemtoReg;     // 1 if data written to register is read from memory or I/O, 0 if data written to register is the output of ALU module
+    output       RegWrite;   	  // 1 if the instruction needs to write to register, 0 otherwise
+    output       MemWrite;       //  1 if the instruction needs to write to memory, 0 otherwise
+    output       Branch;        //  1 if the current instruction is beq, 0 otherwise
+    output       nBranch;       //  1 if the current instruction is bne, 0 otherwise
+    output       Jmp;            //  1 if the current instruction is j, 0 otherwise
+    output       Jal;            //  1 if the current instruction is jal, 0 otherwise
+    output       I_format;      //  1 if the instruction is I-type except for beq, bne, lw and sw; 0 otherwise
+    output       Sftmd;         //  1 if the instruction is a shift instruction, 0 otherwise
+    output[1:0]  ALUOp;        // if the instruction is R-type or I_format, ALUOp's higher bit is 1, 0 otherwise; if the instruction is “beq” or “bne“, ALUOp's lower bit is 1, 0 otherwise
 
     wire lw, sw, beq, bne;
     wire R_format;
 
-    assign R_format = (opcode == 6'b000000) ? 1 : 0;
-    assign lw = (opcode == 6'b100011) ? 1 : 0;
-    assign sw = (opcode == 6'b101011) ? 1 : 0;
-    assign beq = (opcode == 6'b000100) ? 1 : 0;
-    assign bne = (opcode == 6'b000101) ? 1 : 0;
-    assign Jmp = (opcode == 6'b000010) ? 1 : 0;
-    assign Jal = (opcode == 6'b000011) ? 1 : 0;
-    assign Jr = (R_format && (funct == 6'b001000)) ? 1 : 0;
+    assign R_format = (Opcode == 6'b000000) ? 1 : 0;
+    assign lw = (Opcode == 6'b100011) ? 1 : 0;
+    assign sw = (Opcode == 6'b101011) ? 1 : 0;
+    assign beq = (Opcode == 6'b000100) ? 1 : 0;
+    assign bne = (Opcode == 6'b000101) ? 1 : 0;
+    assign Jmp = (Opcode == 6'b000010) ? 1 : 0;
+    assign Jal = (Opcode == 6'b000011) ? 1 : 0;
+    assign Jr = (R_format && (Function_opcode == 6'b001000)) ? 1 : 0;
 
     assign Branch = beq;
     assign nBranch = bne;
@@ -37,11 +35,11 @@ RegDST, MemtoReg, RegWrite, MemWrite, ALUSrc, Sftmd, I_format, ALUOp);
     assign MemtoReg = lw;
     assign RegWrite = ((lw || Jal || R_format || I_format) && !(Jr)) ? 1 : 0;
     assign MemWrite = sw;
-    assign ALUSrc = (lw || sw || I_format) ? 1 : 0; //是否是有立即数操作
-    assign Sftmd = (R_format && ((funct == 6'b000000) || (funct == 6'b000010) 
-                    || (funct == 6'b000100) || (funct == 6'b000110)
-                    || (funct == 6'b000011) || (funct == 6'b000111))) ? 1 : 0;
-    assign I_format = (opcode[5:3] == 3'b001) ? 1 : 0;
+    assign ALUSrc = (lw || sw || I_format) ? 1 : 0;
+    assign Sftmd = (R_format && ((Function_opcode == 6'b000000) || (Function_opcode == 6'b000010) 
+                    || (Function_opcode == 6'b000100) || (Function_opcode == 6'b000110)
+                    || (Function_opcode == 6'b000011) || (Function_opcode == 6'b000111))) ? 1 : 0;
+    assign I_format = (Opcode[5:3] == 3'b001) ? 1 : 0;
     assign ALUOp = {(R_format || I_format), (beq || bne)};
 
 endmodule
--- Decoder.v
+++ Decoder.v
@@ -1,44 +1,38 @@
 `timescale 1ns / 1ps
 
-module Decoder(
-    clk, rst, instruction, 
-    memData, aluResult, regWrite, 
-    memWrite, jal, regDst, addressLink,
-    readData1, readData2, extendedImm
-);
+module decode32(read_data_1, read_data_2, Instruction, mem_data, ALU_result, Jal, RegWrite, MemtoReg, RegDst, Sign_extend, clock, reset, opcplus4);
 
-    input clk, rst;
-    input [31:0] instruction;
-    input [31:0] memData;
-    input [31:0] aluResult;
-    input regWrite, memWrite, jal, regDst; // regWrite-是否写入reg
-    input [31:0] addressLink;//取值单元传给jal指令的地址
+    input clock, reset;
+    input [31:0] Instruction;
+    input [31:0] mem_data;
+    input [31:0] ALU_result;
+    input RegWrite, MemtoReg, Jal, RegDst;
+    input [31:0] opcplus4;
 
-    output [31:0] readData1;
-    output [31:0] readData2;
-    output [31:0] extendedImm;
+    output [31:0] read_data_1;
+    output [31:0] read_data_2;
+    output [31:0] Sign_extend;
 
     reg[31:0] registers[0:31];
 
-    wire R_format = (instruction[31:26] == 6'b0) ? 1 : 0;
-    wire J_format = (instruction[5:0] == 6'b000010 || instruction[5:0] == 6'b000011) ? 1 : 0;
+    wire R_format = (Instruction[31:26] == 6'b0) ? 1 : 0;
+    wire J_format = (Instruction[5:0] == 6'b000010 || Instruction[5:0] == 6'b000011) ? 1 : 0;
     wire I_format = (!R_format && !J_format) ? 1 : 0;
 
-    wire[4:0] rs = instruction[25:21];
-    wire[4:0] rt = instruction[20:16];
-    wire[4:0] rd = instruction[15:11];
-    wire[4:0] writeReg = (jal)? 5'b11111 : (regDst)? rd : rt; // writeReg-要写入的reg
+    wire[4:0] rs = Instruction[25:21];
+    wire[4:0] rt = Instruction[20:16];
+    wire[4:0] rd = Instruction[15:11];
+    wire[4:0] writeReg = (Jal)? 5'b11111 : (RegDst)? rd : rt;
 
-    //判断是否为zero-extension
-    wire is_addiu = (instruction[31:26] == 6'b001001) ? 1 : 0;
-    wire is_sltiu = (instruction[31:26] == 6'b001011) ? 1 : 0;
-    wire is_andi = (instruction[31:26] == 6'b001100) ? 1 : 0;
-    wire is_ori = (instruction[31:26] == 6'b001101) ? 1 : 0;
-    wire is_xori = (instruction[31:26] == 6'b001110) ? 1 : 0;
+    wire is_addiu = (Instruction[31:26] == 6'b001001) ? 1 : 0;
+    wire is_sltiu = (Instruction[31:26] == 6'b001011) ? 1 : 0;
+    wire is_andi = (Instruction[31:26] == 6'b001100) ? 1 : 0;
+    wire is_ori = (Instruction[31:26] == 6'b001101) ? 1 : 0;
+    wire is_xori = (Instruction[31:26] == 6'b001110) ? 1 : 0;
 
     integer i;
-    always @(posedge clk) begin
-        if (rst) begin
+    always @(posedge clock) begin
+        if (reset) begin
           for (i = 0;i < 32;i = i + 1) begin
             registers[i] <= 32'b0;
           end
@@ -46,21 +40,21 @@ module Decoder(
         else begin
         end
 
-        if (regWrite && writeReg) begin
-            if (jal) begin
-                registers[writeReg] = addressLink;
+        if (RegWrite && writeReg) begin
+            if (Jal) begin
+                registers[writeReg] = opcplus4;
             end
-            else if (memWrite) begin
-                registers[writeReg] = memData;
+            else if (MemtoReg) begin
+                registers[writeReg] = mem_data;
             end
             else begin
-                registers[writeReg] = aluResult;
+                registers[writeReg] = ALU_result;
             end
         end
     end
 
-    assign readData1 = registers[rs];
-    assign readData2 = registers[rt];
-    assign extendedImm = (is_addiu || is_sltiu || is_andi || is_ori || is_xori) ? {16'b0, instruction[15:0]} : {{16{instruction[15]}}, instruction[15:0]};
+    assign read_data_1 = registers[rs];
+    assign read_data_2 = registers[rt];
+    assign Sign_extend = (is_addiu || is_sltiu || is_andi || is_ori || is_xori) ? {16'b0, Instruction[15:0]} : {{16{Instruction[15]}}, Instruction[15:0]};
 
 endmodule
