f24d6db515a7f8cee99de546e55c792b053c0030
Yujing <104706509+Yujing27@users.noreply.github.com>
2023-05-17 22:18:36 +0800

    Update CPU_TOP.v

--- CPU_TOP.v
+++ CPU_TOP.v
@@ -1,30 +1,73 @@
 `timescale 1ns / 1ps
 
 module CPU_TOP(
-    input clock, // clk from fpga
-    input rst, // rst from fpga
+    input clock,
+    input rst,
     input[15:0] Switches,
     output[15:0] Lights
-);
+    );
+
+    //clk
     wire cpu_clk;
     wire uart_clk;
+    
+    wire[31:0] instruction;
+
+    wire[31:0] branch_base_addr;
+    wire[31:0] addr_result;
+    wire[31:0] addr_out;  
+    wire[31:0] link_addr; 
+    
+    wire[31:0] read_data_1;
+    wire[31:0] read_data_2;
+    
+    wire Branch,nBranch,Jmp,Jal,Jr,Zero;
+
+    
+    wire[15:0] io_rdata;
+    wire[15:0] io_wdata;
+
+    wire[31:0] mem_data;
+    wire[31:0] alu_result;
+    wire RegWrite,MemtoReg,RegDst;
+    wire sign_extend;
+
+    wire[5:0] opcode;
+    assign opcode = instruction[31:26];
+    wire[5:0] function_opcode;
+    assign function_opcode = instruction[5:0];
+
+    wire ALUSrc,Sftmd;
+    wire I_format;
+    wire[1:0] ALUop;
+    wire[21:0] alu_resultHigh;
+    wire MemorIOtoReg;
+    wire memRead,memWrite,ioRead,ioWrite;
+
+
+    wire[5:0] shamt;
+    assign shamt = instruction[10:6];
+    
+    wire[31:0] PC_plus_4;
+
+    wire[31:0] writeData;
+    wire[31:0] readData;
+
+    wire SwitchCtrl;
+    wire LEDCtrl;
+
+
     clk_wiz_0 clk_instance(
         .clk_in1(clock),
         .clk_out1(cpu_clk),
         .clk_out2(uart_clk)
     );
     
-    wire[31:0] instruction;
-    wire[31:0] branch_base_addr;
-    wire[31:0] addr_result;
-    wire[31:0] read_data_1;
-    wire[31:0] read_data_2;
-    wire Branch,nBranch,Jmp,Jal,Jr,Zero;
-    wire[31:0] link_addr;
+
  
     //Instruction Fetch   
     Ifetc32 if_instance(
-        .Instrucction(instruction),
+        .Instruction(instruction),
         .branch_base_addr(branch_base_addr),
         .Addr_result(addr_result),
         .Read_data_1(read_data_1),
@@ -38,20 +81,10 @@ module CPU_TOP(
         .link_addr(link_addr)
     );
     
-    wire[15:0] io_rdata;
-    wire[15:0] io_wdata;
     
-    Switch switch_instance(
-        .clk(cpu_clk),
-        .rst(rst),
-        .switch_wdata(io_wdata),
-        .switch_rdata(io_rdata)
-    );
+
     
-    wire[31:0] mem_data;
-    wire[31:0] alu_result;
-    wire RegWrite,MemtoReg,RegDst;
-    wire sign_extend;
+    
     
     //CPU Decoder
     decode32 decode_instance(
@@ -68,14 +101,7 @@ module CPU_TOP(
     );
     
     
-    wire[5:0] opcode;
-    wire[5:0] function_opcode;
-    wire ALUSrc,Sftmd;
-    wire I_format;
-    wire[1:0] ALUop;
-    wire[21:0] alu_resultHigh;
-    wire MemorIOtoReg;
-    wire memRead,memWrite,ioRead,ioWrite;
+
     
     
     //CPU Controller
@@ -83,7 +109,7 @@ module CPU_TOP(
         .Opcode(opcode),
         .Function_opcode(function_opcode),
         .Jr(Jr),
-        .RegDst(RegDst),
+        .RegDST(RegDst),
         .ALUSrc(ALUSrc),
         .MemorIOtoReg(MemorIOtoReg),
         .RegWrite(RegWrite),
@@ -94,18 +120,14 @@ module CPU_TOP(
         .I_format(I_format),
         .Sftmd(Sftmd),
         .ALUOp(ALUop),
-        .ALu_resultHigh(alu_resultHigh),
+        .Alu_resultHigh(alu_resultHigh),
         .MemRead(memRead),
         .MemWrite(memWrite),
         .IORead(ioRead),
         .IOWrite(ioWrite)
     );
     
-    wire[5:0] exe_opcode;
-    wire[5:0] shamt;
-    assign shamt = instruction [10:6];
     
-    wire[31:0] PC_plus_4;
     
     //CPU ALU
     executs32 alu_instance(
@@ -113,8 +135,8 @@ module CPU_TOP(
         .Read_data_2(read_data_2),
         .Sign_extend(sign_extend),
         .Function_opcode(function_opcode),
-        .Exe_opcode(exe_opcode),
-        .ALUop(ALUop),
+        .Exe_opcode(opcode),
+        .ALUOp(ALUop),
         .Shamt(shamt),
         .Sftmd(Sftmd),
         .ALUSrc(ALUSrc),
@@ -122,9 +144,7 @@ module CPU_TOP(
         .PC_plus_4(PC_plus_4)
     );
     
-    wire[31:0] writeData;
-    wire[31:0] readData;
-    wire[31:0] addr_out;
+    
     
     dmemory32 dm_instance(
         .clock(cpu_clk),
@@ -134,8 +154,7 @@ module CPU_TOP(
         .readData(readData)
     );
     
-    wire SwitchCtrl;
-    wire LEDCtrl;
+    
     
     MemOrIO morio_instance(
         .mRead(memRead),
@@ -152,5 +171,28 @@ module CPU_TOP(
         .SwitchCtrl(SwitchCtrl),
         .LEDCtrl(LEDCtrl)
     );
+
+
+    Switch switch(
+        .switclk(clock),
+        .switrst(rst),
+        .switchcs(SwitchCtrl),
+        .switchread(ioRead),
+        .switch_wdata(io_rdata),
+        .switch_rdata(ioread_data),
+        .switaddr(addr_out[1:0])
+    );    
+
+
+    ledDriver led(
+        .ledclk(clock),
+        .ledrst(rst),
+        .ledwrite(ioWrite),
+        .ledcs(LEDCtrl),
+        .ledaddr(addr_out[1:0]),
+        .ledinputdata(writeData[15:0]),
+        .ledout(Lights)
+    );
+    
 
 endmodule
