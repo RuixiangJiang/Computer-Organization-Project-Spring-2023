60728035f56455ed5882358ee12af9b588f3e284
Yujing <104706509+Yujing27@users.noreply.github.com>
2023-05-17 21:42:27 +0800

    Update MemOrIO.v

--- basic modules/MemOrIO.v
+++ basic modules/MemOrIO.v
@@ -2,42 +2,37 @@
 
 
 module MemOrIO( mRead, mWrite, ioRead, ioWrite,addr_in, addr_out, m_rdata, io_rdata, r_wdata, r_rdata, write_data, LEDCtrl, SwitchCtrl);
-input mRead; // read memory, fromController
-input mWrite; // write memory, fromController
-input ioRead; // read IO, from Controller
-input ioWrite; // write IO, from Controller
-input[31:0] addr_in; // from alu_result in ALU
-output[31:0] addr_out; // address to Data-Memory
-input[31:0] m_rdata; // data read from Data-Memory
-input[15:0] io_rdata; // data read from IO,16 bits
-output reg[31:0] r_wdata; // data to Decoder(register file)
-input[31:0] r_rdata; // data read from Decoder(register file)
-output reg[31:0] write_data; // data to memory or I/O（m_wdata, io_wdata）output LEDCtrl; // LED Chip Select
-output  SwitchCtrl; // Switch Chip Select
-output LEDCtrl;
+    input mRead; // read memory, fromController
+    input mWrite; // write memory, fromController
+    input ioRead; // read IO, from Controller
+    input ioWrite; // write IO, from Controller
 
-assign addr_out= addr_in;
-// The data wirte to register file may be from memory or io. // While the data is from io, it should be the lower 16bit of r_wdata. assign r_wdata = ？？？
-// Chip select signal of Led and Switch are all active high;
+    input[31:0] addr_in; // from alu_result in ALU
+    output[31:0] addr_out; // address to Data-Memory
 
-always @* begin
-    if((mRead==1)&&(ioRead==0))
-        //wirte_data could go to either memory or IO. where is it from?write_data = ？？？
-        r_wdata = m_rdata;
-    else if ((mRead==0)&&(ioRead!=1))
-        r_wdata = {{16{io_rdata}},io_rdata};
-    else
-        r_wdata = 32'hZZZZZZZZ;
-end
+    input[31:0] m_rdata; // data read from Data-Memory
+    input[15:0] io_rdata; // data read from IO,16 bits
+    output [31:0] r_wdata; // data to Decoder(register file)
 
-assign SwitchCtrl = (ioRead == 1'b1 && addr_in[7:4]==4'h7) ? 1'b1 : 1'b0;
-assign LEDCtrl = (ioWrite == 1'b1&& addr_in[7:4]==4'b0110) ? 1'b1 : 1'b0;
+    input[31:0] r_rdata; // data read from Decoder(register file)
+    output reg[31:0] write_data; // data to memory or I/O锟斤拷m_wdata, io_wdata锟斤拷output LEDCtrl; // LED Chip Select
+    output  SwitchCtrl; // Switch Chip Select
+    output LEDCtrl;
 
-always @* begin
-    if((mWrite==1)||(ioWrite==1))
-    //wirte_data could go to either memory or IO
-        write_data = addr_out;
-    else
-        write_data = 32'hZZZZZZZZ;
-end
+    assign addr_out= addr_in;
+    // The data wirte to register file may be from memory or io. // While the data is from io, it should be the lower 16bit of r_wdata. assign r_wdata = 锟斤拷锟斤拷锟斤拷
+    // Chip select signal of Led and Switch are all active high;
+
+    assign r_wdata = (mRead == 1)? m_rdata:{{16{io_rdata[15]}},io_rdata};
+
+    assign SwitchCtrl = (ioRead == 1'b1 && addr_in[7:4]==4'h7) ? 1'b1 : 1'b0;
+    assign LEDCtrl = (ioWrite == 1'b1&& addr_in[7:4]==4'b0110) ? 1'b1 : 1'b0;
+
+    always @* begin
+        if((mWrite==1)||(ioWrite==1))
+        //wirte_data could go to either memory or IO
+            write_data = r_rdata;
+        else
+            write_data = 32'hZZZZZZZZ;
+    end
 endmodule
