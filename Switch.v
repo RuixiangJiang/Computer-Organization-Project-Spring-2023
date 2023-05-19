74f0fac2c370eb3259abb394398996f18ca1bcab
SnowCharm <619022098@qq.com>
2023-05-20 01:54:10 +0800

    bug fix Switch variable name

--- Switch.v
+++ Switch.v
@@ -10,7 +10,7 @@ module Switch(
     input[1:0] switaddr
 );
     always @ (negedge switclk or posedge switrst) begin
-        if(rst) switch_wdata <= 0;
+        if(switrst) switch_wdata <= 0;
 		else if (switcs && switread) begin
             if (switaddr == 2'b00) switch_wdata[15:0] <= switch_rdata[15:0]; // data output, lower 16 bits non-extended
 			else if (switaddr == 2'b10) switch_wdata[15:0] <= {8'h00, switch_rdata[15:8]}; // data output, upper 8 bits extended with zero
