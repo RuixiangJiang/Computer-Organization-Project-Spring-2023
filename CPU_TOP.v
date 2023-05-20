76600759e3a3fe1a395cfb4bd24a30837410d87b
RuixiangJiang <2037358823@qq.com>
2023-05-20 18:11:16 +0800

    upd uart

--- CPU_TOP.v
+++ CPU_TOP.v
@@ -4,12 +4,15 @@ module CPU_TOP(
     input clock,
     input rst,
     input[15:0] Switches,
-    output[15:0] Lights
-    );
+    output[15:0] Lights,
+    // Uart
+    input iStartRecvCoe,
+    input iFpgaUartFromPC,
+    output oFpgaUartToPC
+);
 
     //clk
     wire cpu_clk;
-    wire uart_clk;
     
     wire[31:0] instruction;
 
@@ -56,14 +59,52 @@ module CPU_TOP(
 
     wire SwitchCtrl;
     wire LEDCtrl;
+    wire UartCtrl;
 
+
+    // Uart
+    wire[15:0] uartData;
+    wire upgclk;
+    wire upgclk_o;
+    wire upgWriteOutEnable;
+    wire upgInputDataFinish; // iFpgaUartFromPC finish
+    wire[14:0] upgAddrMemUnit; // data to which memory unit of rom/dmemory
+    wire[31:0] upgDataTo; // data to rom or Dmemory
+    wire spg_bufg;
+    BUFG U1(.I(iStartRecvCoe), .O(spg_bufg)); // de-twitter
+    reg upg_rst; // generate uart rst signal
+    always @(posedge clock) begin
+        if (spg_bufg) upg_rst = 0;
+        if (rst) upg_rst = 1;
+    end
+    wire not_uart_rst = rst | !upg_rst;
+    wire kickOff = upg_rst | (~upg_rst & upgInputDataFinish);
+    // CPU works on normal/uart mode when kickOff = 1/0
+    uart_bmpg_0 uartinst(
+        .upg_clk_i(upgclk),
+        .upg_rst_i(upg_rst),
+        .upg_rx_i(iFpgaUartFromPC),
+        .upg_clk_o(upgclk_o),
+        .upg_wen_o(upgWriteOutEnable),
+        .upg_adr_o(upgInputDataFinish),
+        .upg_dat_o(upgDataTo),
+        .upg_done_o(upgInputDataFinish)
+    );
+    uartDriver uartdriverins(
+        .iFpgaClock(clock),
+        .iCpuClock(cpu_clk),
+        .iCpuReset(not_uart_rst),
+        .iUartCtrl(UartCtrl),
+        .iIoRead(IORead),
+        .iUartFromPc(iFpgaUartFromPC),
+        .oUartData(uartData)
+    )
 
     clk_wiz_0 clk_instance(
         .clk_in1(clock),
         .clk_out1(cpu_clk),
-        .clk_out2(uart_clk)
+        .clk_out2(upgclk)
     );
-    
 
  
     //Instruction Fetch   
@@ -79,7 +120,7 @@ module CPU_TOP(
         .Jr(Jr),
         .Zero(Zero),
         .clock(cpu_clk),
-        .reset(rst),
+        .reset(not_uart_rst),
         .link_addr(link_addr)
     );
     
@@ -101,7 +142,7 @@ module CPU_TOP(
         .RegDst(RegDst),
         .Sign_extend(sign_extend),
         .clock(cpu_clk),
-        .reset(rst),
+        .reset(not_uart_rst),
         .opcplus4(PC_plus_4)
     );
     
@@ -178,23 +219,25 @@ module CPU_TOP(
         .r_rdata(read_data_2),
         .write_data(writeData),
         .SwitchCtrl(SwitchCtrl),
-        .LEDCtrl(LEDCtrl)
+        .LEDCtrl(LEDCtrl),
+        //.UartCtrl(UartCtrl)
     );
 
-
+    wire[15:0] switch_wdata;
     Switch switch(
         .switclk(clock),
-        .switrst(rst),
+        .switrst(not_uart_rst),
         .switcs(SwitchCtrl),
         .switread(ioRead),
-        .switch_wdata(io_rdata),
+        .switch_wdata(switch_wdata),
         .switch_rdata(Switches)
     );    
+    assign io_rdata = UartCtrl ? uartData : switch_wdata;
 
 
     ledDriver led(
         .ledclk(clock),
-        .ledrst(rst),
+        .ledrst(not_uart_rst),
         .ledwrite(ioWrite),//ioWrite
         .ledcs(LEDCtrl),//LEDCtrl
         .ledaddr(addr_out[1:0]),
