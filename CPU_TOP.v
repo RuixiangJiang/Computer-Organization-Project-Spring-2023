f0ff5c820f171f9e39e96abcb38fd2dd30c15cc0
Yujing <104706509+Yujing27@users.noreply.github.com>
2023-05-21 00:43:04 +0800

    Update CPU_TOP.v

--- CPU_TOP.v
+++ CPU_TOP.v
@@ -5,13 +5,21 @@ module CPU_TOP(
     input rst,
     input[15:0] Switches,
     output[15:0] Lights,
-    input start_pg,
+    input start_pg_button,
     input rx,
     output tx
 );
 
     //clk
     wire cpu_clk;
+    wire start_pg;
+
+    buttonDriver bd(
+        .clk_i(cpu_clk),
+        .rst_n_i(rst),
+        .key_i(start_pg_button),
+        .poweron(start_pg)
+    );
 
     wire[31:0] instruction;
 
