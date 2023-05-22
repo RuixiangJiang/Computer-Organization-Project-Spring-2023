7acb32f33a816f3171473639d2f199dbe0040117
Yujing <104706509+Yujing27@users.noreply.github.com>
2023-05-22 14:43:07 +0800

    Update ledDriver.v

--- basic modules/ledDriver.v
+++ basic modules/ledDriver.v
@@ -6,16 +6,16 @@ module ledDriver(
     input ledwrite,
     input ledcs, // led chip-select
     input[1:0] ledaddr,
-    input[15:0] ledinputdata,
+    input[7:0] ledinputdata,
     output reg[15:0] ledout
 );
     always @(posedge ledclk or posedge ledrst) begin
         if (ledrst) ledout <= 16'h0;
         else if (ledcs && ledwrite) begin
-            if(ledaddr == 2'b00)
-                ledout[7:0] <= ledinputdata[7:0];
-            else if(ledaddr === 2'b10)
-                ledout[15:8] <= ledinputdata[7:0];
+            if(ledaddr == 2'b00) // 0xFFFFFC60
+                ledout[7:0] <= ledinputdata;
+            else if(ledaddr === 2'b10) // 0xFFFFC62
+                ledout[15:8] <= ledinputdata;
             else
                 ledout <= ledinputdata;
         end
