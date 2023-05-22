e004910325ad5c1343aa99d9309732687ac3f47f
Yujing <104706509+Yujing27@users.noreply.github.com>
2023-05-22 14:43:50 +0800

    Update MemOrIO.v

--- basic modules/MemOrIO.v
+++ basic modules/MemOrIO.v
@@ -23,7 +23,7 @@ module MemOrIO( mRead, mWrite, ioRead, ioWrite,addr_in, addr_out, m_rdata, io_rd
     // The data wirte to register file may be from memory or io. // While the data is from io, it should be the lower 16bit of r_wdata. assign r_wdata = 锟斤拷锟斤拷锟斤拷
     // Chip select signal of Led and Switch are all active high;
 
-    assign r_wdata = (mRead == 1)? m_rdata:{{16{io_rdata[15]}},io_rdata}; // TODO
+    assign r_wdata = (mRead == 1)? m_rdata:{{24{io_rdata[15]}},io_rdata}; // TODO
 
     assign SwitchCtrl = ioRead;
     assign LEDCtrl = ioWrite;
