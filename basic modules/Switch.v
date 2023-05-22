fdb4fa0b58dde8cd7a329d166455fdef9d5d73f0
Yujing <104706509+Yujing27@users.noreply.github.com>
2023-05-22 15:41:21 +0800

    Update Switch.v

--- basic modules/Switch.v
+++ basic modules/Switch.v
@@ -7,15 +7,18 @@ module Switch(
     input[1:0] switchaddr,
     input switread, // read-signal
     output reg[7:0] switch_wdata, // 16 bit data write to CPU
-    input[15:0] switch_rdata // 16 bit data read from ego1
+    input[15:0] switch_rdata, // 16 bit data read from ego1
+    input check_button
 );
     always @ (negedge switclk or posedge switrst) begin
         if(switrst) switch_wdata <= 0;
 		else if (switcs && switread) begin
             if(switchaddr == 2'b00)
                 switch_wdata <=switch_rdata[7:0];
-            else if(switchaddr === 2'b10)
+            else if(switchaddr == 2'b10)
                 switch_wdata <= switch_rdata[15:8];
+            else if(switchaddr == 2'b11)
+                switch_wdata <= {7'b0,check_button};
             else
             switch_wdata <= switch_rdata;
 		end
